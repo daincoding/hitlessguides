92417a9f252f37ba2eb969b797b31af036be6632
dain. <officialdain.music@gmail.com>
2024-12-16 14:28:07 +0100

    Adding Grids

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { Box } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Box>Hello from App</Box>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // 1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="red" hideBelow="lg">
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="green">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
